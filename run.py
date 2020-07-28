4c9602d92cfc6eb91b219eb9ec3709ae22ebd2d8
kj-jondell <kj@jondell.com>
2020-07-28 14:05:58 +0200

    vad gör vi

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from src.a import *
 
-
 def b(x):
     if isinstance(x, list):
         x.append(x)
     else:
         x = [x]
+
+o = list(str(A("hej axel!")))
+b(o) # test av b
+print(o)
---
+++ src/a.py
@@ -0,0 +1,7 @@
+class A:
+
+    def __init__(self, str = ""):
+        self.str = str
+
+    def __str__(self):
+        return self.str
